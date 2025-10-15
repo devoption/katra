4020ae63ffc3624615c3d6c331b8e930f65f0b75
Derek Bourgeois <dbourgeois23@gmail.com>
2025-10-15 06:36:55 -0400

    chore: remove SSL workarounds now that herd secure is configured

--- config/reverb.php
+++ config/reverb.php
@@ -34,13 +34,7 @@ return [
             'path' => env('REVERB_SERVER_PATH', ''),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => env('REVERB_TLS_CERT') && env('REVERB_TLS_KEY') ? [
-                    'local_cert' => env('REVERB_TLS_CERT'),
-                    'local_pk' => env('REVERB_TLS_KEY'),
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                    'allow_self_signed' => true,
-                ] : [],
+                'tls' => [],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
