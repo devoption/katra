0b7237436552450a0466ffce81794712a1838352
Derek Bourgeois <dbourgeois23@gmail.com>
2026-03-19 09:18:44 -0400

    test: stabilize desktop shell runtime assertions

--- tests/Feature/DesktopShellTest.php
+++ tests/Feature/DesktopShellTest.php
@@ -2,6 +2,9 @@
 
 test('the desktop shell exposes the katra bootstrap screen', function () {
     config()->set('surreal.autostart', false);
+    config()->set('surreal.host', '127.0.0.1');
+    config()->set('surreal.port', 18999);
+    config()->set('surreal.endpoint', 'ws://127.0.0.1:18999');
 
     $this->get('/')
         ->assertSuccessful()
